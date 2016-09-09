36a094ba1d29827f8bacdfe53e75692560385e16
Vladimir Sagan <vlasad@gmail.com>
2016-09-09 17:55:28 +0300

    fix tests and migration template

--- lib/gorm-goose/dbconf_test.go
+++ lib/gorm-goose/dbconf_test.go
@@ -13,26 +13,12 @@ func TestBasics(t *testing.T) {
 	}
 
 	got := []string{dbconf.MigrationsDir, dbconf.Env, dbconf.Driver.Name, dbconf.Driver.OpenStr}
-	want := []string{"../../db-sample/migrations", "test", "postgres", "user=liam dbname=tester sslmode=disable"}
+	want := []string{"../../db-sample/migrations", "test", "mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local"}
 
 	for i, s := range got {
 		if s != want[i] {
 			t.Errorf("Unexpected DBConf value. got %v, want %v", s, want[i])
 		}
-	}
-}
-
-func TestImportOverride(t *testing.T) {
-
-	dbconf, err := NewDBConf("../../db-sample", "customimport", "")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	got := dbconf.Driver.Import
-	want := "github.com/custom/driver"
-	if got != want {
-		t.Errorf("bad custom import. got %v want %v", got, want)
 	}
 }
 
